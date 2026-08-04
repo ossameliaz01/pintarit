5bab4e966037fb4911c4898261f1166a36b0f27b
ossameliaz-crypto <ossameliaz@student.ub.ac.id>
2026-08-04 23:00:42 +0700

    Siap deploy dengan data hardcode

--- app/Http/Controllers/Api/DictionaryController.php
+++ app/Http/Controllers/Api/DictionaryController.php
@@ -11,7 +11,15 @@ class DictionaryController extends Controller
 {
     public function index()
     {
-        $terms = Dictionary::all();
+        // Hardcoded data for fast/free deployment without DB
+        $terms = [
+            ['id' => 1, 'term' => 'HTML', 'definition' => 'HyperText Markup Language, the standard markup language for documents designed to be displayed in a web browser.'],
+            ['id' => 2, 'term' => 'CSS', 'definition' => 'Cascading Style Sheets, used for describing the presentation of a document written in HTML.'],
+            ['id' => 3, 'term' => 'JavaScript', 'definition' => 'A programming language that conforms to the ECMAScript specification, often used for web development.'],
+            ['id' => 4, 'term' => 'API', 'definition' => 'Application Programming Interface, a way for two or more computer programs to communicate with each other.'],
+            ['id' => 5, 'term' => 'Database', 'definition' => 'An organized collection of data, generally stored and accessed electronically from a computer system.'],
+        ];
+
         return response()->json($terms);
     }
 }
