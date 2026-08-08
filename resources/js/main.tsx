a1688e221b7e8fa1b6ce4c3f8484a6a0abeef3c9
ossameliaz-crypto <ossameliaz@student.ub.ac.id>
2026-08-08 11:59:54 +0700

    feat: initialize main React entry point and configure axios with Laravel CSRF and Sanctum credentials

--- resources/js/main.tsx
+++ resources/js/main.tsx
@@ -7,13 +7,19 @@ import App from './App'
 import '../css/app.css' 
 import axios from 'axios'
 
+declare global {
+    interface Window {
+        axios: any;
+    }
+}
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true; // Penting untuk Sanctum/Session Cookie Auth
 
 // Get CSRF token from meta tag
-const token = document.head.querySelector('meta[name="csrf-token"]');
+const token = document.head.querySelector('meta[name="csrf-token"]') as HTMLMetaElement | null;
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
